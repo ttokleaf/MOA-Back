fad9ac834058bdc790420f7b4135e1e8ba8383ad
yong <jk07028@naver.com>
2025-11-10 20:10:34 +0900

    first commit

---
+++ src/main/java/com/moa/back/MoaBackApplication.java
@@ -0,0 +1,13 @@
+package com.moa.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moa/back/MoaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moa.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
